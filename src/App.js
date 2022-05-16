9a17d14b75c88d919f61693fb3e1140e4e755043
coderyeasin <yeasin3233@gmail.com>
2022-05-16 15:49:35 +0600

    board

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-          </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-
   );
 }
 
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-  return <div>
-    <h3>Dashboard</h3>
-  </div>;
+  return (
+    <div>
+      <div className="user_info gap-3">
+        <div className="aside_menu">
+          <nav>
+            <ul>
+              <li className="my-3 text-success p-2 text-justify rounded-3 border-none">
+                Dashboard
+              </li>
+              <li className="my-3 text-success p-2 text-justify rounded-3 border-none">
+                User info
+              </li>
+              <li className="my-3 text-success p-2 text-justify rounded-3 border-none">
+                Profile
+              </li>
+              <li className="my-3 text-success p-2 text-justify rounded-3 border-none">
+             Settings
+              </li>
+              <li className="my-3 text-success p-2 text-justify rounded-3 border-none">
+            Logout
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="content_here">
+          <h3>Bar Chart</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
