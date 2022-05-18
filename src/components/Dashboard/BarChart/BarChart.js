91e24391049f64ef52c7800f033cbca942f80181
coderyeasin <yeasin3233@gmail.com>
2022-05-18 12:43:45 +0600

    tooltip

--- src/components/Dashboard/BarChart/BarChart.js
+++ src/components/Dashboard/BarChart/BarChart.js
@@ -2,24 +2,21 @@ import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 
 const BarChart = () => {
-
   const barChart = useRef();
   const randomNums = 300;
-  let data = []
+  let data = [];
 
   for (let i = 0; i <= randomNums; i++) {
     // console.log(data = randomNums)
-    data.push(Math.random() * randomNums.toFixed(2))
+    data.push(Math.random() * randomNums.toFixed(2));
   }
-// console.log(data)
-const highData = (data.filter((e) => e >= 200))
-  const catched = highData.slice(0, 31)
+  // console.log(data)
+  const highData = data.filter((e) => e >= 200);
+  const catched = highData.slice(0, 31);
   console.log(catched);
-data = catched;
-  
+  data = catched;
 
   useEffect(() => {
-
     // setting up svg
     let width = 700;
     let height = 300;
@@ -29,8 +26,8 @@ data = catched;
       .attr("height", height)
       .style("overflow", "visible")
       .style("margin-top", "75px")
-      .style("margin-left", "75px")
- 
+      .style("margin-left", "75px");
+
     //   .style("background", "gray");
 
     //setting the scaling
@@ -54,23 +51,30 @@ data = catched;
       .data(data)
       .join("rect")
       .attr("x", (v, i) => xScale(i))
-        .attr("y", yScale)
-        .attr('width', xScale.bandwidth())
-      .attr('height', val => height - yScale(val))
-  }, [data]);
+      .attr("y", yScale)
+      .attr("width", xScale.bandwidth())
+      .attr("height", (val) => height - yScale(val))
+      .attr("fill", "#6C63FF")
+      .append("title")
+      .text((d) => d.toFixed(2))
     
-  
+  }, [data]);
 
   //button
   const handleRandom = () => {
-      //press to value change //refresh page
+    window.location.reload();
     //hover to show what is evary bar number
   };
 
   return (
     <div>
       <svg ref={barChart}></svg> <br />
-      <button onClick={handleRandom} className="border-0 px-3 py-2 bg-primary text-light fs-4 text-capitalize my-5">Press</button>
+      <button
+        onClick={handleRandom}
+        className="border-0 px-3 py-2 bg-primary text-light fs-4 text-capitalize my-5"
+      >
+        Press
+      </button>
     </div>
   );
 };
