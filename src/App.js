295c3dcb6f1b50ff16b6a6c8643854f2175704af
coderyeasin <yeasin3233@gmail.com>
2022-05-20 18:51:03 +0600

    secure

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import Home from "./components/Home/Home";
 import Login from "./components/Register/Login";
 import Register from "./components/Register/Register";
+import PrivateRoute from "./components/Security/PrivateRoute";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- src/components/Security/PrivateRoute.js
+++ src/components/Security/PrivateRoute.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import { useLocation, Navigate } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            <h3>Secure your Route</h3>
-        </div>
-    );
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user, isErrors } = useAuth();
+  const location = useLocation();
+
+//   if (isErrors) {
+//     return (
+//       <div>
+//         <Spinner animation="border" role="status">
+//           <span className="visually-hidden">Loading...</span>
+//         </Spinner>
+//       </div>
+//     );
+//   }
+
+  if (user?.email) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} />;
 };
 
 export default PrivateRoute;
